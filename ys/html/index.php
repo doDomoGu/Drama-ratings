75c4a67d7fc2865be68d56f297ce881839dd94a9
doDomoGu <kaka_71936410@qq.com>
2017-11-07 15:16:53 +0800

    每个页面 获取不同的集数

--- ys/html/index.php
+++ ys/html/index.php
@@ -22,32 +22,35 @@
 
     $dramas = $dbh->query('SELECT d.id,d.title,ts.name as `tv`, pt.name as `time`, d.rating_avg, d.trend from `drama` d join tv_station ts on d.tv_id = ts.id join play_time pt on d.time_id = pt.id where page_id = '.$_GET['p_id']);
 
-
+    $epi_num = $dbh->query('SELECT max(e.num) from `episode` e join `drama` d on e.drama_id = d.id where e.enable = 1 and d.page_id = '.$_GET['p_id'])->fetchColumn();
+    
     ?>
     <div><?=$page['year'].' / '.$season[$page['season']]?></div>
-    <ul style="list-style:none;font-weight:bold;">
+    <ul style="list-style:none;padding-left:0;font-weight:bold;">
         <li>
+            <span style="display:inline-block;width:30px;">#</span>
             <span style="display:inline-block;width:400px;">剧名</span>
             <span style="display:inline-block;width:60px;">电视台</span>
             <span style="display:inline-block;width:60px;">时段</span>
 
-            <?php for($i=1;$i<=11;$i++):?>
+            <?php for($i=1;$i<=$epi_num;$i++):?>
             <span style="display:inline-block;width:60px;"><?=$i?></span>    
             <?php endfor;?>
             <span style="display:inline-block;width:60px;">平均</span>
             <span style="display:inline-block;width:100px;">走势</span>
         </li>
     </ul>
-    <ul>
+    <ul style="list-style:none;padding-left:0;">
         <?php foreach($dramas as $d):?>
             <?php 
                 $episodes = $dbh->query('SELECT `num`,`rating` from `episode` where drama_id = '.$d['id'].' and `enable` = 1')->fetchAll();
             ?>
             <li style="height:36px;">
+                <span style="display:inline-block;width:30px;"><?=$d['id']?></span>
                 <span style="display:inline-block;width:400px;"><?=$d['title']?></span>
                 <span style="display:inline-block;width:60px;"><?=$d['tv']?></span>
                 <span style="display:inline-block;width:60px;"><?=$d['time']?></span>
-                <?php for($i=0;$i<11;$i++):?>
+                <?php for($i=0;$i<$epi_num;$i++):?>
                     <span style="display:inline-block;width:60px;"><?=isset($episodes[$i])?$episodes[$i]['rating']:''?></span>    
                 <?php endfor;?>
                 <span style="display:inline-block;width:60px;"><?=$d['rating_avg']?></span>
